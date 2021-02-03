f8b287a3f3a94d84bc571068bb8e7d95c638b73a
MdNahid360 <mdnahid360s@gmail.com>
2021-02-03 01:38:57 -0800

    writing code

---
+++ main.js
@@ -0,0 +1,17 @@
+// const user = {
+//     id: 245,
+//     name:"md nahid",
+//     friend: {
+//         name:"murshed",
+
+//     },
+//     friendAge : [12,45,55],
+//     friendList : ["Murshed", "Ferdaos", "Shamim"];
+// };
+// const userJSON = JSON.stringify(user);
+// console.log(userJSON);
+
+// const userFromJSON = JSON.parse(userJSON);
+// console.log(userFromJSON);
+
+console.log('lololo');
